92410703e583e7b20e5efe25c656d3b467611da8
Chris <sears.topher@gmail.com>
2019-02-15 16:38:29 -0500

    server config

--- index.js
+++ index.js
@@ -4,5 +4,16 @@ const app = express();
 
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve production assets (main.js || main.css)
+    app.use(express.static('client/build'));
+
+    // Express will serve index.html if it doesnt recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
